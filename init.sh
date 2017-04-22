9896aef5312935f1e5dc0b216cea4a6cd043434f
Vladislav Shatilenko <shatilenko.v@gmail.com>
2017-04-22 19:04:10 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
